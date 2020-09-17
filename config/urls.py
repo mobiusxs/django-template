0de67fcaeebdebf314716777c76d6d361186f916
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-09-17 01:23:27 -0500

    store admin path in environ

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
     path('', include('index.urls'), name='index'),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns.insert(0, path('__debug__/', include(debug_toolbar.urls)), )
+    urlpatterns.insert(0, path('__debug__/', include(debug_toolbar.urls)))
